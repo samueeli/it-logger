a88212f0bac604b47a8565988c8715b583d40596
samueeli <samuli.salminen@raivo.io>
2021-06-21 11:59:15 +0300

    added AddBtn component

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
 
 import SearchBar from './components/layout/SearchBar';
-import Logs from './components/logs/Logs'
+import Logs from './components/logs/Logs';
+import AddBtn from './components/layout/AddBtn';
 
 const App = () => {
   useEffect(() => {
@@ -16,10 +17,11 @@ const App = () => {
     <Fragment>
       <SearchBar />
       <div className="container">
+        <AddBtn />
         <Logs />
       </div>
     </Fragment>
-  )
+  );
 };
 
 export default App;
---
+++ src/components/layout/AddBtn.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const AddBtn = () => {
+  return (
+    <div className="fixed-action-btn">
+      <a href="#add-log-modal" className="btn-floating btn-large blue darken-2 modal-trigger">
+        <span className="large material-icons">add</span>
+      </a>
+      <ul>
+        <li>
+          <a href="#tech-list-modal" className="btn-floating green modal-trigger">
+            <span className="material-icons">person</span>
+          </a>
+        </li>
+        <li>
+          <a href="#tech-modal" className="btn-floating red modal-trigger">
+            <span className="material-icons">person_add</span>
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default AddBtn;
