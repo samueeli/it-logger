d9fd34595954e9fd2940398badb04673cb1e6281
samueeli <samuli.salminen@raivo.io>
2021-06-21 11:40:44 +0300

    added data to logItems and a Preloder for the logs loading

--- src/components/logs/LogItem.js
+++ src/components/logs/LogItem.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import Moment from 'react-moment';
 import PropTypes from 'prop-types';
 
 const LogItem = ({ log }) => {
   return (
     <li className="collection-item">
       <div>
-        <a href="">{log.message}</a>
+        <a href="#edit-log-modal" className={`modal-trigger ${log.attention ? 'red-text' : 'blue-text'}`}>
+          {log.message}
+        </a>
+        <br />
+        <span className="grey-text">
+          <span className="black-text">ID #{log.id} </span>
+          last updated by{' '}
+          <span className="black-text">{log.tech} </span>on{' '}
+          <Moment format='MMMM Do YYYY, h:mm a'>{log.date}</Moment>
+        </span>
+        <a href="#!" className="secondary-content">
+          <span className="material-icons grey-text">delete</span>
+        </a>
       </div>
     </li>
   );
